491857a55e2ccb471c460fc0da4648102fe0d396
Edd1e <estebanrco@hotmail.com>
2022-08-27 08:57:26 -0500

    final user

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "db1",
+        "USER": "esteban",
+        "PASSWORD": "calixtoesgay",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
